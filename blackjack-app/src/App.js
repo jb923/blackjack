c668d931d8272fb90c01169418a8defb5fba70bf
Johnny <jbui923@gmail.com>
2024-02-27 17:11:35 -0800

    add deal cards to appjs

--- blackjack-app/src/App.js
+++ blackjack-app/src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from 'react';
+import { createDeck, calculateScore, checkGameStatus } from './components/gameLogic.js';
+
 function App() {
+  const [deck, setDeck] = useState(createDeck());
+  cosnt [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+  const [gameStatus, setGameStatus] = useState('');
+
+  const dealCards = () => {
+    const newDeck = [...deck];
+    const playerCards = [newDeck.pop(), newDeck.pop()];
+    const dealerCards = [newDeck.pop(), newDeck.pop()];
+    setDeck(newDeck);
+    setPlayerHand(playerCards);
+    setDealerHand(dealerCards);
+    setGameStatus('');
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
